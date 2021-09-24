6c9129bc17a64aa69dae3a6912066dd538dc1406
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-23 22:12:02 -0400

    update gitignore

--- client/store.js
+++ client/store.js
@@ -1,5 +1,6 @@
 
- //just added , needs to be further updated / imports
+ //just added, needs to be further updated / imports
+
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
   }

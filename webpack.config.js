1a1056426a79c4dd699b1ee02ef52c58db1fdb9d
J-Mad-08 <maddox.version1@gmail.com>
2021-09-25 16:39:42 -0400

    commit update to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'bundle.js',
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/build/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    proxy: {
+      "/api": "http://localhost:3000",
     },
-    devServer: {
-        proxy: {
-            '/api': 'http://localhost:3000',
+    static: {
+      directory: __dirname,
+    },
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
         },
-        static: {
-            directory: __dirname,
-        },
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-              test: /\.jsx?/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env','@babel/preset-react'],
-                  plugins: ['@babel/plugin-proposal-object-rest-spread']
-                }
-              }
-            },
-        ]
-    },
+      },
+    ],
+  },
 };

af7dd554a86f7d49d1ce27e327c2d5c229c4291a
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-25 13:57:14 -0400

    client/index

--- client/index.js
+++ client/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-<<<<<<< HEAD
-ReactDOM.render(
-        <Provider store={store}>
-            {/* <h1>React Hello World</h1> */}
-            <App/>
-        </Provider>,
-        document.getElementById('app')
-=======
 render(
     <h1>React Hello World</h1>,
     document.getElementById('app')
->>>>>>> 91717341cce4bc00d86aa70d1d6464278aa38305
 );

96dd94902740a74b67fd5384da2150e7185a2333
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-25 17:45:19 -0400

    updates to login component with css loaders

---
+++ client/components/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login(props) {
+    return (
+    <div className="loginMain">
+        <div>
+            <b>Welcome to Your Travel Planner</b>
+            
+        </div>
+        
+    </div>
+    
+    )
+  }
+  
+
+  export default Login;

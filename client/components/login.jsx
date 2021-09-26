94f7db4cf89be802dc8674473b9cf8f0048d3780
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-26 14:16:31 -0400

    input fields and button without styling

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 
 function Login(props) {
     return (
-    <div className="loginMain">
+    <div className='loginMain'>
+         <p class='loginHeading'>Welcome to Your Travel Planner</p>
         <div>
-            <b>Welcome to Your Travel Planner</b>
-            
+            <form type='text'> 
+                <div class='userInput'>
+                    <label for='userName'>User Name:</label><br/>
+                    <input type='text'></input><br/>
+                    <button class='button'></button><br/>
+                    <label for='userPass'>User Pass:</label><br/>
+                    <input type='text'></input><br/>
+                    <button class='button'></button>
+                </div>
+                <div class='loginSubHeading'>
+                    <p>Forgot password?</p>
+                    <p>Register</p>
+                </div>
+            </form>
         </div>
-        
     </div>
     
     )

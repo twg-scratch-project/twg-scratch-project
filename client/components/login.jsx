b316b142940423697b446872de4ed31ba7f4205f
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-26 20:44:08 -0400

    login component designed will update with button finctionality  and client/index color sheet update

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
+//research importing an img file for logo
+//import '../TP_Logo/TravelPlanner_Logo/TravelPlanner_Logo.png';
 
 function Login(props) {
     return (
-    <div className='loginMain'>
-         <p class='loginHeading'>Welcome to Your Travel Planner</p>
-        <div>
+    <div >
+        <h1>Welcome to Your Travel Planner</h1>
+        <span></span>
+        <img src=''/>
+        <div className='loginMain'>
             <form type='text'> 
-                <div class='userInput'>
+                <div className='userInput'>
+                    <h4>Travel Planner</h4>
                     <label for='userName'>User Name:</label><br/>
                     <input type='text'></input><br/>
-                    <button class='button'></button><br/>
-                    <label for='userPass'>User Pass:</label><br/>
+                    {/* <button className='button'></button><br/> */}
+                    <label for='userPass'>User Pass:</label><br/> 
                     <input type='text'></input><br/>
-                    <button class='button'></button>
-                </div>
-                <div class='loginSubHeading'>
-                    <p>Forgot password?</p>
-                    <p>Register</p>
-                </div>
+                    <p><button className='SubmitButton'>Submit</button></p>
+                        <div className='loginSubHeading'>
+                            <button style={{borderWidth:'0px'}}>Forgot password? </button><br/>
+                            <button style={{borderWidth:'0px'}}>Register</button>
+                        </div>
+                </div> 
+                {/* <div className='loginSubHeading'>
+                    <span>Forgot password?</span>
+                    <span>Register</span>
+                </div> */}
             </form>
         </div>
     </div>

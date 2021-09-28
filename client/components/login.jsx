fd850166cceb3184220301109f9d18e68cfb9281
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-27 20:36:19 -0400

    capturing user input of login page w useState

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,32 +1,55 @@
-import React from 'react';
-//research importing an img file for logo
+import React, {useState, useEffect} from 'react';
+//research importing an img file for logo!
 //import '../TP_Logo/TravelPlanner_Logo/TravelPlanner_Logo.png';
 
+
 function Login(props) {
+    // const [password, setPassword] = useState('');
+
+    // const userNameEntry = (event) => {
+    //     console.log(setUserName(event.target.value));
+    // }
+
+    function userPassEntry (event) {
+        props.onChange(event.target.value);
+        console.log('password ', event.target.value)
+    }
+
+    function userNameEntry (event) {
+        props.onChange(event.target.value);
+        console.log('password ', event.target.value)
+    }
+
+    //login button function
+    const submitLogin = () => {
+        const userDetails = {
+            'user name': userName,
+            'user password': password
+        }
+        console.log('userdeets ',userDetails)
+    }
+
     return (
     <div >
         <h1>Welcome to Your Travel Planner</h1>
         <span></span>
         <img src=''/>
         <div className='loginMain'>
+            {/* //do a need an action prop here and a page? */}
             <form type='text'> 
                 <div className='userInput'>
                     <h4>Travel Planner</h4>
-                    <label for='userName'>User Name:</label><br/>
-                    <input type='text'></input><br/>
-                    {/* <button className='button'></button><br/> */}
-                    <label for='userPass'>User Pass:</label><br/> 
-                    <input type='text'></input><br/>
-                    <p><button className='SubmitButton'>Submit</button></p>
+                    <label for='html'>Username/Email:</label><br/>
+                    <input type='text' userName={props.userName} onChange={userNameEntry} placeholder='Enter User Name'/><br/>
+                    <label for='pass'>Password:</label><br/> 
+                    <input type='text' id='pass' password={props.password} onChange={userPassEntry} placeholder='Enter Password'/><br/>
+                        <p><button className='SubmitButton' onClick={submitLogin}>Submit</button></p>
                         <div className='loginSubHeading'>
-                            <button style={{borderWidth:'0px'}}>Forgot password? </button><br/>
-                            <button style={{borderWidth:'0px'}}>Register</button>
+                            <button style={{border:'none', backgroundColor:'whiteSmoke'}}>Forgot password? 
+                            </button><br/>
+                            <button style={{border:'none'}}>Register</button>
                         </div>
                 </div> 
-                {/* <div className='loginSubHeading'>
-                    <span>Forgot password?</span>
-                    <span>Register</span>
-                </div> */}
             </form>
         </div>
     </div>

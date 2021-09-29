bf2179c83d24e33dbc98ba5a35921b3eaca458ad
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-28 20:17:29 -0400

    client files

--- client/components/Registration.jsx
+++ client/components/Registration.jsx
@@ -5,21 +5,36 @@ import React from 'react';
 import style from '../index.css';
 
 function Registration(props) {
+  //needs to be coded with props and functionality 
+
+  const regSubmit = () => {
+    const regDetails = {
+        'first name': userName,
+        'last password': password,
+        'email': email
+    }
+    console.log('reg deets ',regDetails)
+  }
     return (
       <div>
         <h1>Travel Planner Registration</h1>
-        <div className='document'>
-          <form>
+        <div className="document">
+          <form action="" method="post">
             <div>
               <h2>Travel Planner Registration</h2>
-              <label for="firstname">First Name: </label>
-              <input type="text" name="firstname" placeholder="First Name"></input><br/>
-              <label for="lastname">Last Name: </label>
+              <p>
+                <label for="firstname"></label>
+                <input type="text" name="firstname" placeholder="First Name"></input><br/>
+              </p>
+              <p>
+              <label for="lastname"> </label>
               <input type="text" name="lastname" placeholder="Last Name"></input><br/>
-              <label for="lastname">Email: </label>
-              <input type="text" email="email" placeholder="Email"></input>
+              </p>
+              <label for="lastname"></label>
+              <input type="email" email="email" placeholder="Email"></input>
+              <p></p>
+              <button className="SubmitButton" onClick={regSubmit}>Submit</button>
             </div>
-  
           </form>
         </div>
       </div>

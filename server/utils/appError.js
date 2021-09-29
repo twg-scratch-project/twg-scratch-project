d13587e88115b52568a14c0d0c58456f86f60154
samj01 <4789992+samj01@users.noreply.github.com>
2021-09-28 18:37:26 -0700

    Added partial controller for the trip

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,10 +1,11 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, raw) {
     super(message);
 
     this.statusCode = statusCode;
     this.status == `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
+    this.raw = raw;
 
     Error.captureStackTrace(this, this.constructor);
   }

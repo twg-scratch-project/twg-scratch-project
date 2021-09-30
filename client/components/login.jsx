7d78712c66510e16f2d47cc0ccea5920004bb0c2
Rachel Collins <spiritedndiligent@gmail.com>
2021-09-30 19:17:03 -0400

    updating login form onChange functions

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -4,37 +4,27 @@ import React, {useState, useEffect} from 'react';
 
 
 function Login(props) {
-    // const [password, setPassword] = useState('');
-
+    const [password, setPassword] = useState('');
+    const [name, setUserName] = useState('');
     // const userNameEntry = (event) => {
     //     console.log(setUserName(event.target.value));
     // }
 
-    function userPassEntry (event) {
-        //add validation/if event is not a string...
-        props.onChangePass(event.target.value);
-        console.log('password ', event.target.value)
-    }
-
-    function userNameEntry (event) {
-        //add validation/if event is not a string...
-        props.onChangeUser(event.target.value);
-        console.log('password ', event.target.value)
-    }
-
-    //login button function
-    // const submitLogin = () => {
-    //     const userDetails = {
-    //         'user name': userName,
-    //         'user password': password
-    //     }
-    //     console.log('userdeets ',userDetails)
+    // function userPassEntry (event) {
+    //     //add validation/if event is not a string...
+    //     props.onChange(event.target.value);
+    //     console.log('password ', props.onChange(event => event.target.value))
     // }
-    //func for submit?
-    //onClick={submitLogin}
+    // function userNameEntry (event) {
+    //     //add validation/if event is not a string...
+    //     props.onChange(event.target.value);
+    //     console.log('password ', event.target.value)
+    // }
+
     //invoked with forms request to server
     const myFunc = (e) => {
         e.preventDefault();
+        console.log('name and pass ', name, password)
         fetch('/users/verifyUser', {
             method: 'POST',
             mode: 'cors',
@@ -43,30 +33,30 @@ function Login(props) {
             },
             body: JSON.stringify(
                 {
-                name: props.name,
-                password: props.password
+                name: name,
+                password: password
                 })
         })
-        .then(response => {console.log('fetch response ', response)})
-        .then()
+        .then(response => response.json())
+        .then( data => console.log(data))
+        .catch(() => console.log('fetch error has occurred'))
     }
-
     return (
-    <div >
+    <div>
         <h1>Welcome to Your Travel Planner</h1>
-        <span></span>
         {/* travel planner logo */}
         <img src=''/>
         <div className='loginMain'>
             {/* /* //needs to be a fetch request */}
             <form  method='POST'> 
                 <div className='userInput'>
-                    <h3>Travel Planner</h3>
+                    <h3>Travel Journal</h3>
                     <label for='html'></label><br/>
-                    <input type='text' value={props.name} onChange={props.onChangeUser} placeholder='Enter User Name'/><br/>
+                    <input type='text' value={name} onChange={e => setUserName(e.target.value)} placeholder='Enter User Name'/><br/>
                     <label for='pass'></label><br/> 
-                    <input type='password' id='pass' value={props.password} onChange={props.onChangePass} placeholder='Enter Password'/><br/>
-                        <p><button className='SubmitButton' onClick={myFunc} >Submit</button></p>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} 
+                    placeholder='Enter Password'/><br/>
+                        <p><button className='SubmitButton' onClick={myFunc}>Submit</button></p>
                         <div className='loginSubHeading'>
                             <button style={{border:'none', fontSize:'10px'}}>Forgot password? 
                             </button><br/>

2aafaede51b844270860c19b0e60c71094d8ba1d
Yochi Shor <moebochur@gmail.com>
2021-10-01 12:17:01 -0400

    Mounted a route for deleting a comment

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -5,4 +5,6 @@ const router = express.Router({ mergeParams: true });
 
 router.post('/', commentController.addComment);
 
+router.delete('/:id', commentController.deleteComment);
+
 module.exports = router;

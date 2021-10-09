70a3364c97c36d6ab25106bcdc551fc9b2e8efed
hi2dmitri <idmytro@yahoo.com>
2021-10-09 10:32:21 -0400

    apiController and Router trips endpoints fixed

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,5 +1,7 @@
 const db = require('../config/db');
 const User = require('../models/Models');
+const mongoose = require("mongoose");
+
 const apiController = {};
 
 apiController.createUser = async (req, res, next) => {
@@ -13,6 +15,7 @@ apiController.createUser = async (req, res, next) => {
         return next(err);
     }
 }
+
 apiController.findUser = async (req, res, next) => {
     try {
         const {email} = req.params;
@@ -22,6 +25,59 @@ apiController.findUser = async (req, res, next) => {
     }
     catch (err) {
         return next(err);
+    }
+}
+
+apiController.getTrips = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const trips = await User.findById(mongoose.Types.ObjectId(id)).select("trips").sort({startDate: 1})
+        const todayDate = new Date();
+        const pastTrips = [];
+        const upcomingTrips = [];
+        for (let i = 0; i < trips.trips.length; i++) {
+        const startDate = new Date(trips.trips[i].startDate);
+        if (todayDate > startDate) {pastTrips.push(trips.trips[i])}
+            else {upcomingTrips.push(trips.trips[i])}
+
+        }
+        req.body.trips = {id: trips._id.toString(), pastTrips, upcomingTrips};
+        return next();
+    }
+    catch (err) {
+      next(err)
+    }
+}
+
+apiController.addTrip = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const {trip} = req.body; 
+        //const user = await User.findOne({_id: mongoose.Types.ObjectId(id)});
+        //await user.trips.push(trip);
+        //await user.save();
+        await User.updateOne({_id: mongoose.Types.ObjectId(id)}, {"$push": {
+            "trips" : {
+                "$each" : [trip],
+                "$sort" : {"startDate" : 1}
+            }
+        }})
+        return next();
+    }
+    catch (err) {
+        return next(err)
+    }
+}
+
+apiController.deleteTrip = async (req, res, next) => {
+    try {
+       const {id} = req.params;
+       const {trip_id} = req.body;
+       await User.findOneAndUpdate({_id: mongoose.Types.ObjectId(id)}, {$pull: { trips : {_id: mongoose.Types.ObjectId(trip_id)}}} )
+       return next();
+    }
+    catch (err) {
+        return next(err)
     }
 }
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -6,4 +6,20 @@ router.get('/getuser/:email', apiController.findUser, (req, res, next) => {
   const user = res.locals.user;
   res.status(200).json({user});
 })
+
+router.get('/gettrips/:id', apiController.getTrips, (req, res, next) =>  {
+const trips = req.body.trips;
+   res.status(200).json(trips);
+})
+
+router.put('/addtrip/:id', apiController.addTrip, apiController.getTrips, (req, res, next) => {
+    const trips = req.body.trips;
+    res.status(200).json(trips);
+})
+
+router.put('/deletetrip/:id', apiController.deleteTrip, apiController.getTrips, (req, res, next) => {
+    const trips = req.body.trips;
+    res.status(200).json(trips);
+})
+
 module.exports = router;

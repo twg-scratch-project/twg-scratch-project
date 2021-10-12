e6755086e2d63c2f61a380d8ba4c751c57b585bf
Rachel Collins <spiritedndiligent@gmail.com>
2021-10-12 00:31:52 -0400

    preliminary login

--- client/components/test.jsx
+++ client/components/test.jsx
@@ -1,17 +1,30 @@
 //import logo from './images/journalLogo.png';
-import React, {useState, useEffect, useCallback} from 'react';
-import {useHistory} from 'react-router-dom';
-import style from '../index.css';
-import { Grid, TextField, Button, Paper, Card, CardContent } from '@material-ui/core';
-import CardHeader from '@mui/material/CardHeader';
+import * as React from 'react';
+// import {useHistory} from 'react-router-dom';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import style from '../index.css';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Box from '@mui/material/Box';
+// import { Grid, TextField, Button, Paper, Card, CardContent } from '@material-ui/core';
+// import CardHeader from '@mui/material/CardHeader';
 // import Typography from '@mui/material/Typography';
-// import DeleteIcon from '@mui/icons-material/Delete';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import logo from '../images/journalLogo.png';
-import { makeStyles } from '@material-ui/core/styles';
-// // import { ViewHeadlineTwoTone } from '@mui/icons-material';
-// import { ClassNames } from '@emotion/react';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 // Still updating this component and the others!
 
 // const useStyles = makeStyles(theme => ({
@@ -34,88 +47,111 @@ import { makeStyles } from '@material-ui/core/styles';
 //   }
 // }))
 
+
+function Copyright(props) {
+  
+
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        Team Toe Shoes
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
 function test(props) {
-  // const regComp = () => {
-  //   //render reg component onClick
-  // }
 
-  // const classes = useStyles;
-  // const loginUser = (username, password) => {
-  //   fetch("/api/auth", {
-  //     method: "POST",
-  //     mode: "cors",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       email: username,
-  //       password: password,
-  //     }),
-  //   })
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log("Data received: ", data);
-  //     dispatch({
-  //       type: types.SET_CURRENT_USER,
-  //       payload: {
-  //         name: data.data.user.name,
-  //         email: data.data.user.email,
-  //         id: data.data.user._id,
-  //       },
-  //     });
-  //   })
-  //   .catch((e) => console.log(e));
+  function SignInSide() {
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      // eslint-disable-next-line no-console
+      console.log({
+        email: data.get('email'),
+        password: data.get('password'),
+      });
+    };
+  }
 
-  // };
-
-    return (
-      <form>
-        <Paper>
-        {/* <Grid container className={classes.root}> */}
-        <Grid container >
-          {/* <Grid item className={classes.image}/> */}
-          <Grid item />
-          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={4} square>Login</Grid>
-          <TextField
-          required
-          variant="outlined"
-          fullWidth
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          margin="normal"
-          autofocus
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-          </TextField>
-          <TextField
-          required
-          variant="outlined"
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          margin="normal"
-          autoComplete="current-password"
-          >
-          </TextField>
-          <Button variant="outlined"
-          fullWidth
-          color="primary"
-          variant="contained"
-          style={{margin:"6"}}
-          >Login</Button>
-          <Button variant="outlined"
-          style={{margin:"6"}}
-          fullWidth
-          color="primary"
-          variant="contained"
-          // onClick={regComp}
-          >Register</Button>
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar> */}
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={SignInSide} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    CREATE AN ACCOUNT
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
         </Grid>
-      </Paper>
-      </form>
-
-    )
+      </Grid>
+    </ThemeProvider>
+  );
   }
   //conditionally render this component based on user selection?
   // function Registration(props) {

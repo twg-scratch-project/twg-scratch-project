d34a71538ae96ed473c713d1b70570aeb22c21e4
Eric Mulhern <eric.mulhern@gmail.com>
2021-10-13 19:09:07 -0400

    Cleaned up unused code & comments in AddTrip & TripDetail components.

--- client/components/AddTrip.jsx
+++ client/components/AddTrip.jsx
@@ -1,26 +1,17 @@
 import React, { useState, useEffect } from "react"; 
 // import logo from '../images/journalLogo';
           
-function addTrip(props, setAllTrips) {
+function AddTrip(selected, setAllTrips) {
   const [tripName, setTripName] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
-
-  const [notes, setNotes] = useState("");
-  // const [newTrip, setNewTrip] = useState({});
-  const [number, setNumber] = useState(null);
-  // const date = useSelector((state) => state.date);
-
-  // const [deleter, entryDeleter] = useState('');
-  // const [editor, entryEditor] = useState('');
-
-  let renderTripDetailOrAddTrip;
+  const [newTrip, setNewTrip] = useState({});
 
   const handleSubmission = (e) => { // moved from Main
     e.preventDefault();
     console.log('submitted', newTrip);
     // add new journal entry to DB
-    fetch(`/api/addTrip`, {
+    fetch(`/api/addtrip/${USER_ID}`, {
       method: 'POST',
       mode: 'cors',
       headers: {
@@ -36,56 +27,11 @@ function addTrip(props, setAllTrips) {
     });
   };
 
-
   return (
-    // <form> 
-    //   <div className="currentEntry"> {/* input field to add new journal entry. neeeds to be combined with input fields for date & trip duration. */}
-    //         <textarea className="cell" value={notes} onChange={e => props.setNotes(e.target.value)}>
-    //           New Entry
-    //         </textarea><br/>
-    //         <button class='SubmitButton' onClick={handleSubmission}>Submit Entry </button> {/*on submission, also include lat&long values in body */}
-    //         <br/>
-    //     </div>
-
-    //     <div class="itinerary">
-    //     <div>
-    //       <p>
-    //         <form>
-    //           <label for="">Where Do You Want to Go?</label>
-
-    //           <input
-    //             type="text"
-    //             className="cell"
-    //             onChange={(e) => setTripName(e.target.value)}
-    //           />
-    //           <br />
-    //           <br />
-    //           <label htmlFor="">When Are You Leaving?</label>
-    //           <br />
-    //           <br />
-    //           <label for=""> When are you returning?</label>
-    //           <input
-    //             type="number"
-    //             formMethod="post"
-    //             value={number}
-    //             onChange={(e) => startDate(e.target.value)}
-    //           />
-    //           <br />
-    //           <br />
-    //           <input type="date" />
-    //           <br />
-    //           <br />
-    //           <button onClick={handleSubmission}>Lets Go!</button>
-    //         </form>
-    //       </p>
-    //     </div>
-    //   </div>
-
-
-    // </form> 
+    
     <div>Hello this is the AddTrip component, thanks</div>
   )
   
 }        
             
-export default addTrip;
+export default AddTrip;
--- client/components/TripDetail.jsx
+++ client/components/TripDetail.jsx
@@ -5,18 +5,22 @@ import React, { useState, useEffect } from "react";
 
 function TripDetail({curSelectedTrip}) { // props = current selected trip object
   // const [tripCoordinates, updateCoordinates] = useState('');
-  //const handleSubmission = e => {};
-  
-  // SELECT DEFAULT VALUES WHEN NO TRIP IS PRESENT
-
-
+  //const handleSubmission = e => {}; 
   const { locationName, coordinates, startDate, endDate, description} = curSelectedTrip;
-
 
   return (
     <div className="allEntries"> {/*instead of rendering all entries, make it render only the entry corresponding to currently selected pin on map */}  
       <div className="cell" className='divScroll'>
       {locationName} 
+      </div>
+      <div className="cell" className='divScroll'>
+      {startDate} 
+      </div>
+      <div className="cell" className='divScroll'>
+      {endDate} 
+      </div>
+      <div className="cell" className='divScroll'>
+      {description} 
       </div>
   </div>
   )
